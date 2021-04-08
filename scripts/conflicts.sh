d529539cd7a6503817f77fc10e814501cbab4db8
de13 <stephane.beuret@data-essential.com>
2021-04-08 15:33:39 +0200

    typo

--- scripts/conflicts.sh
+++ scripts/conflicts.sh
@@ -2,10 +2,11 @@
 
 git checkout -b green 
 echo "# super dumper commit" > newfile.txt
-echo -e "\n#I created the newfile.txt to solve the issue" >> README.md
+git add newfile.txt
+echo "I created the newfile.txt to solve the issue" >> README.md
 git commit -a -m "solevd teh issue with a newfiel"
 git checkout -b newbranch
-echo -e "\nEverything looks fine now" >> README.md
+echo "Everything looks fine now" >> README.md
 git commit -a -m "nothing here, just to say"
 git checkout master
 git merge green
